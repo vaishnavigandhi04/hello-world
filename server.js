f7520dd13a1d401797a65157f35b8e3bc46b5f14
vaishnavigandhi04 <vaishnavigandhi04@gmail.com>
2019-03-25 16:06:29 -0700

    my second commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8080;
+
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -75,7 +77,6 @@ app.get('/404', (request, response) => {
         error: 'Page not found'
     })
 });
-
-app.listen(8080, () => {
-    console.log('Server is up on the port 8080');
+app.listen(port, () => {
+    console.log(`Server is up on the port ${port}`);
 });
