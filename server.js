b2c5973cda78e0b0756fda17fcdf17cddb4e9162
vaishnavigandhi04 <vaishnavigandhi04@gmail.com>
2019-03-25 15:32:07 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('message', (text) => {
+    return text.toUpperCase();
+});
+
+// // app.use((request, response, next) => {
+//     var time = new Date().toString();
+//     //console.log(`${time}: ${request.method} ${request.url}`);
+//     var log = `${time}: ${request.method} ${request.url}`;
+//     fs.appendFile('server.log', log + '\n', (error) => {
+//         if (error) {
+//             console.log('Unable to log message');
+//         }
+//     });
+//     next();
+//
+// });
+
+app.use((request, response, next) => {
+    // var time = new Date().toString();
+    // //console.log(${time}: ${request.method} ${request.url});
+    // var log = `${time}: ${request.method} ${request.url}`;
+    // fs.appendFile('server.log', log + '\n', (error) => {
+    //     if (error) {
+    //         console.log('Unable to log message');
+    //     }
+    // });
+
+    response.render('construction.hbs', {
+        title: 'Maintenence',
+        year: new Date().getFullYear(),
+        welcome: 'Sorry for the inconvenience !'
+    });
+
+
+    //next();
+});
+
+app.get('/', (request, response) => {
+    // response.send('<h1>Hello Express!</h1>');
+    response.send({
+        name: 'Vaishnavi',
+        school: [
+            'BCIT',
+            'SFU',
+            'UBC'
+        ]
+    })
+});
+
+
+
+app.get('/info', (request, response) => {
+    response.render('about.hbs' , {
+        title: 'About page',
+        year: new Date().getFullYear(),
+        welcome: 'Hello!'
+    });
+});
+
+app.get('/404', (request, response) => {
+    response.send({
+        error: 'Page not found'
+    })
+});
+
+app.listen(8080, () => {
+    console.log('Server is up on the port 8080');
+});
